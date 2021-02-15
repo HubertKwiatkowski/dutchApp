34bd1d6df983aca2024e42138be113d4586d7246
warlock <h.kwiatkowski@hotmail.com>
2021-02-15 05:50:06 +0100

    minor changes

--- dutchApp/main.py
+++ dutchApp/main.py
@@ -1,7 +1,14 @@
 import sys
+import kivy
 
 
-class DutchApp():
+class DutchApp:
     """Main class to run app."""
-    pass
 
+    def __init__(self):
+        """Initializing the app."""
+        pass
+
+    def runApp(self):
+        """The main loop of the app"""
+        pass
